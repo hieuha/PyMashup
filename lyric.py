cb62e435612682e7b89f64b6087cc2672d1399be
harry <hieuht@vnoss.org>
2016-04-19 01:53:51 +0900

    lyrics processing

--- lyric.py
+++ lyric.py
@@ -1,8 +1,32 @@
+import glob
+import re
+
+
 class Lyric(object):
 
     """docstring for Lyric"""
 
-    def __init__(self, lyrics):
-        self.lyrics = lyrics
+    def __init__(self, in_dir='output'):
+        self.in_dir = in_dir
+        self.lyrics = glob.glob(self.in_dir+'/*.lrc')
 
+    def get_time(self, line):
+        re_t = re.search(r'\[(.+?)\](.*)\[(.+?)\](.*)', line)
+        return "%s %s\n" % (re_t.group(1), re_t.group(3))
 
+    def search(self, pattern):
+        f_w = open(self.in_dir+'/times.txt', 'w')
+        for lyric in self.lyrics:
+            print 'Processing %s' % lyric
+            file_mp3 = lyric.replace('.lrc', '.mp3')
+            with open(lyric, 'r') as f:
+                for _ in xrange(7):  # Pass Header Zing
+                    next(f)
+                for line in f:
+                    if pattern in line:
+                        start = line.strip()
+                        stop = f.next().strip()
+                        line = '%s %s' % (start, stop)
+                        f_w.write(file_mp3+'|'+self.get_time(line))
+            f.close()
+        f_w.close()
--- test.py
+++ test.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python
+# coding:utf-8
 
-import zingmp3
-import download
+# import zingmp3
+# import download
 
-zing = zingmp3.ZingMp3()
+# zing = zingmp3.ZingMp3()
+# songs = zing.top_song(3)
+# infos = zing.songs_info(songs)
+# my_download = download.Download(infos)
+# my_download.start()
 
-songs = zing.top_song(3)
-infos = zing.songs_info(songs)
-my_download = download.Download(infos)
-my_download.start()
+import lyric
+
+my_lyric = lyric.Lyric()
+my_lyric.search('tình yêu')
